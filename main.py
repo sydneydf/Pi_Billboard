e4fc095e4e90a572ce2219fb80b53bc9a08db8f5
sydneydf <sydneydelafuente@gmail.com>
2022-02-25 14:22:04 +0800

    Numerous Comments and cleanup

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import time
+from turtle import width
 from digitalio import DigitalInOut
 import board
 from dotenv import load_dotenv, find_dotenv
-from PIL import Image, GifImagePlugin
-from adafruit_rgb_display.st7735 import ST7735
+from PIL import Image, GifImagePlugin, ImageOps
+from adafruit_rgb_display.st7735 import ST7735R
 
 load_dotenv(find_dotenv())
 
@@ -20,22 +21,24 @@ BAUDRATE: int = 64000000
 spi: board = board.SPI()
 
 #Setup Display object
-disp: ST7735 = ST7735(spi, width=128, height=128, rotation=270, cs=cs_pin, dc=dc_pin, rst=rst_pin, baudrate=BAUDRATE)
-
+disp: ST7735R = ST7735R(spi, width=128, height=128, rotation=270, cs=cs_pin, dc=dc_pin, rst=rst_pin, baudrate=BAUDRATE)
 #Open Up Gif
 gif: GifImagePlugin = Image.open('monkey_360_128.gif')
 
 while True:
     print(f"Start of gif loading {gif.n_frames}")
     try:
+        #Iterate Over all frames
         for frame_num in range(gif.n_frames):
+            #Set next frame for showing via seek method
             gif.seek(frame_num)
-            # print(type(gif))
             print(f"Playing Frame Number: {frame_num}")
             #Convert PIL Image (Frame) to RGB and set the display to the current image
+            # disp.image(ImageOps.pad(gif.convert("RGB"), size=(128,128), method=Image.NEAREST, centering=(0.5, 0.5)))
             disp.image(gif.convert("RGB"))
     except Exception as E:
+        #Catch exception and restart OS
         print(f"Exception caught: {E}. Waiting 30 seconds and then rebooting")
-        # time.sleep(30)
-        # os.system('reboot')
+        time.sleep(30)
+        os.system('reboot')
 
